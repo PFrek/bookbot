fa7316d744c27335d27123e2b6aad38a53090cf3
PFrek <pedrogayaf@gmail.com>
2024-05-12 21:19:50 -0300

    Add report output

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 def main():
-    with open("books/frankenstein.txt") as f:
+    filepath = "books/frankenstein.txt"
+    with open(filepath) as f:
         contents = f.read()
         word_count = get_word_count(contents)
-        print(f"Words: {word_count}")
-
         letter_count = get_letter_counts(contents)
-        print(f"Letter count:")
-        print(letter_count)
+
+        print("--- Begin report of books/frankenstein.txt ---")
+        print(f"{word_count} words found in the document")
+        print()
+
+        print_letter_count(letter_count)
+        print("--- End report ---")
 
 
 def get_word_count(text):
@@ -23,5 +27,24 @@ def get_letter_counts(text):
 
     return letters
 
+def convert_to_letter_list(dict):
+    letters_list = []
+    
+    for letter in dict:
+        if letter.isalpha():
+            letters_list.append({ "letter": letter, "count": dict[letter]})
+    
+    return letters_list
+    
+def sort_on(dict):
+    return dict["count"]
+
+def print_letter_count(dict):
+    letters_list = convert_to_letter_list(dict)
+    
+    letters_list.sort(reverse=True, key=sort_on)
+
+    for entry in letters_list:
+        print(f"The '{entry["letter"]}' character was found {entry["count"]} times")
 
 main()
